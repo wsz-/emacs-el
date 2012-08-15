1b843d6cee02ea60c1bc408df833fa0aa65f5687
ivy <cisir92@gmail.com>
2012-08-15 15:57:05 +0800

    emacs p

---
+++ wsz.el
@@ -0,0 +1,28 @@
+(defun copy-line (&optional arg)
+  "Save current line into Kill-Ring without mark the line"
+  (interactive "P")
+  (let ((beg (line-beginning-position)) 
+	(end (line-end-position arg)))
+    (copy-region-as-kill beg end))
+  )
+(defun comment-line-or-uncomment-it (&optional arg)
+  "×¢ÊÍ»ò·´×¢ÊÍnÐÐ"
+  (interactive "P")
+  (let ((beg (line-beginning-position))
+	(end (line-end-position)))
+    (comment-or-uncomment-region beg end))
+  )
+(defun copy-word (&optional arg)
+  "Copy words at point"
+  (interactive "P")
+  (let ((beg (progn (if (looking-back "[a-zA-Z0-9]" 1) (backward-word 1)) (point))) 
+	(end (progn (forward-word arg) (point))))
+    (copy-region-as-kill beg end))
+  )
+(defun copy-paragraph (&optional arg)
+  "Copy paragraphes at point"
+  (interactive "P")
+  (let ((beg (progn (backward-paragraph 1) (point))) 
+	(end (progn (forward-paragraph arg) (point))))
+    (copy-region-as-kill beg end))
+  )
